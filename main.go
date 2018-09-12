720232ee467a0cad991b18b84c7418f59873ca3f
xiaocong <xiaocong@gmail.com>
2018-09-12 11:36:23 +0800

    rm redundent code

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ package main
 
 import (
 	"os"
-	"os/signal"
 	"runtime"
 
 	"github.com/BOXFoundation/Quicksilver/cmd"
@@ -27,15 +26,13 @@ var RootProcess goprocess.Process
 var logger *log.Logger
 
 func init() {
-	RootProcess = goprocess.WithParent(goprocess.Background())
+	RootProcess = goprocess.WithSignals(os.Interrupt)
 	logger = log.NewLogger("main")
 }
 
 // start node server
 func startNodeServer(cfg *config.Config) error {
 	log.Setup(cfg) // setup logger
-
-	interruptListener() // listenning OS Signals
 
 	var host, err = p2p.NewDefaultHost(RootProcess, cfg.ListenAddr, cfg.ListenPort)
 	if err != nil {
@@ -54,34 +51,14 @@ func startNodeServer(cfg *config.Config) error {
 	}
 
 	select {
+	case <-RootProcess.Closing():
+		logger.Info("Box server is shutting down...")
+	}
+
+	select {
 	case <-RootProcess.Closed():
 		logger.Info("Box server is down.")
 	}
 
 	return nil
-}
-
-// interruptListener listens for OS Signals such as SIGINT (Ctrl+C) and shutdown
-// requests via func ContexCancel.
-func interruptListener() {
-	// interruptSignals defines the default signals to catch in order to do a proper
-	// shutdown.  This may be modified during init depending on the platform.
-	var interruptSignals = []os.Signal{os.Interrupt}
-
-	go func() {
-		interruptChannel := make(chan os.Signal, 1)
-		signal.Notify(interruptChannel, interruptSignals...)
-
-		// Listen for initial shutdown signal and close the returned
-		// channel to notify the caller.
-		for {
-			select {
-			case sig := <-interruptChannel:
-				logger.Infof("Received signal (%s). Shutting down...", sig)
-				RootProcess.Close()
-			case <-RootProcess.Closed():
-				return
-			}
-		}
-	}()
 }
