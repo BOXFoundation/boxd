ff8902d797a26b638ac6896ff92be0462d6fb810
xiaocong <xiaocong@gmail.com>
2018-09-14 01:42:30 +0800

    update glide.lock

--- p2p/connmanager.go
+++ p2p/connmanager.go
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-// Copyright (c) 2018 ContentBox Authors.
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE file.
-
 package p2p
 
 import (
