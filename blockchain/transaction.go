3425d45e1dd527d164bde757baeb6d4e5c6732e6
Xiaohui <whulxh@gmail.com>
2018-09-13 18:11:13 -0700

    add blockchain data structure (draft)

---
+++ blockchain/transaction.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2018 ContentBox Authors.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+package types
+
+import (
+	"github.com/BOXFoundation/Quicksilver/crypto"
+)
+
+// Transaction defines a transaction.
+type Transaction struct {
+    version  int32
+    vin     []*TxIn
+    vout    []*TxOut
+    lockTime uint32
+}
+
+// TxOut defines a transaction output.
+type TxOut struct {
+    value    int64
+    scriptPubKey []byte
+}
+
+// TxIn defines a transaction input.
+type TxIn struct {
+    prevOutPoint OutPoint
+    scriptSig  []byte
+    sequence         uint32
+}
+
+// OutPoint defines a data type that is used to track previous transaction outputs.
+type OutPoint struct {
+    hash  crypto.HashType
+    index uint32
+}
+
+// mempool
+map<crypto.HashType, Transaction*> hashToTx;
+
+// orphan transaction pool
+map<crypto.HashType, Transaction*> hashToOrphanTx;
+// orphan transaction's parent; one parent can have multiple orphan children
+multimap<crypto.HashType, Transaction*> parentToOrphanTx;
+
+// UTXO set
+map<OutPoint, TxOut> outpointToOutput;
