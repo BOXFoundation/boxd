cad158aa77d58baaee1e5196316f8497cdbe324e
Leon <leon@castbox.fm>
2018-09-28 14:58:00 +0800

    core: add simple bitcoin address

---
+++ core/types/address.go
@@ -0,0 +1,94 @@
+// Copyright (c) 2018 ContentBox Authors.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package types
+
+import (
+	"errors"
+
+	"github.com/btcsuite/btcutil/base58"
+	"golang.org/x/crypto/ripemd160"
+)
+
+// Address is an interface type for any type of destination a transaction
+// output may spend to.
+type Address interface {
+	// String returns the string encoding of the transaction output
+	// destination.
+	//
+	// Please note that String differs subtly from EncodeAddress: String
+	// will return the value as a string without any conversion, while
+	// EncodeAddress may convert destination types (for example,
+	// converting pubkeys to P2PKH addresses) before encoding as a
+	// payment address string.
+	String() string
+
+	// EncodeAddress returns the string encoding of the payment address
+	// associated with the Address value.  See the comment on String
+	// for how this method differs from String.
+	EncodeAddress() string
+
+	// ScriptAddress returns the raw bytes of the address to be used
+	// when inserting the address into a txout's script.
+	ScriptAddress() []byte
+}
+
+// AddressPubKeyHash is an Address for a pay-to-pubkey-hash (P2PKH)
+// transaction.
+type AddressPubKeyHash struct {
+	hash  [ripemd160.Size]byte
+	netID byte
+}
+
+// NewAddressPubKeyHash returns a new AddressPubKeyHash.  pkHash mustbe 20
+// bytes.
+func NewAddressPubKeyHash(pkHash []byte, netID byte) (*AddressPubKeyHash, error) {
+	return newAddressPubKeyHash(pkHash, netID)
+}
+
+// newAddressPubKeyHash is the internal API to create a pubkey hash address
+// with a known leading identifier byte for a network, rather than looking
+// it up through its parameters.  This is useful when creating a new address
+// structure from a string encoding where the identifer byte is already
+// known.
+func newAddressPubKeyHash(pkHash []byte, netID byte) (*AddressPubKeyHash, error) {
+	// Check for a valid pubkey hash length.
+	if len(pkHash) != ripemd160.Size {
+		return nil, errors.New("pkHash must be 20 bytes")
+	}
+
+	addr := &AddressPubKeyHash{netID: netID}
+	copy(addr.hash[:], pkHash)
+	return addr, nil
+}
+
+// EncodeAddress returns the string encoding of a pay-to-pubkey-hash
+// address.  Part of the Address interface.
+func (a *AddressPubKeyHash) EncodeAddress() string {
+	return encodeAddress(a.hash[:], a.netID)
+}
+
+// ScriptAddress returns the bytes to be included in a txout script to pay
+// to a pubkey hash.  Part of the Address interface.
+func (a *AddressPubKeyHash) ScriptAddress() []byte {
+	return a.hash[:]
+}
+
+// String returns a human-readable string for the pay-to-pubkey-hash address.
+// This is equivalent to calling EncodeAddress, but is provided so the type can
+// be used as a fmt.Stringer.
+func (a *AddressPubKeyHash) String() string {
+	return a.EncodeAddress()
+}
+
+// Hash160 returns the underlying array of the pubkey hash.  This can be useful
+// when an array is more appropiate than a slice (for example, when used as map
+// keys).
+func (a *AddressPubKeyHash) Hash160() *[ripemd160.Size]byte {
+	return &a.hash
+}
+
+func encodeAddress(hash160 []byte, netID byte) string {
+	return base58.CheckEncode(hash160[:ripemd160.Size], netID)
+}
