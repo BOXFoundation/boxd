853ebd3002697804f9d8336275104dbb04b711d4
Leon <leon@castbox.fm>
2018-10-17 22:02:26 +0800

    merge develop to dpos

--- boxd/server.go
+++ boxd/server.go
@@ -147,10 +147,9 @@ func (server *Server) Start(v *viper.Viper) error {
 	peer.Run()
 	blockChain.Run()
 	txPool.Run()
-	// if cfg.Dpos.EnableMint {
-	// 	consensus.Run()
-	// }
-	consensus.Run()
+	if cfg.Dpos.EnableMint {
+		consensus.Run()
+	}
 
 	// goprocesses dependencies
 	//            root
