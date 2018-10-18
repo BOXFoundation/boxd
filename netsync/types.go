32ddc66277f72cf81620f4961c2975a703480ae7
Jerry Peen <zhiguo.ping@castbox.fm>
2018-10-18 15:07:13 +0800

    chore(netsync): implement Marshal and Unmarshal methods for six messages
    the six messages is: LocateHeaders, SyncHeaders, CheckHash, SyncCheckHash,
    FetchBlocksHeaders, SyncBlocks
    
    working #65

--- netsync/types.go
+++ netsync/types.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 
+	corepb "github.com/BOXFoundation/boxd/core/pb"
 	coreTypes "github.com/BOXFoundation/boxd/core/types"
 	"github.com/BOXFoundation/boxd/crypto"
 	netsyncpb "github.com/BOXFoundation/boxd/netsync/pb"
@@ -14,6 +15,19 @@ import (
 var (
 	ErrEmptyProtoMessage   = errors.New("Empty proto message")
 	ErrInvalidProtoMessage = errors.New("Invalid proto message")
+
+	_ conv.Convertible  = (*LocateHeaders)(nil)
+	_ conv.Serializable = (*LocateHeaders)(nil)
+	_ conv.Convertible  = (*SyncHeaders)(nil)
+	_ conv.Serializable = (*SyncHeaders)(nil)
+	_ conv.Convertible  = (*CheckHash)(nil)
+	_ conv.Serializable = (*CheckHash)(nil)
+	_ conv.Convertible  = (*SyncCheckHash)(nil)
+	_ conv.Serializable = (*SyncCheckHash)(nil)
+	_ conv.Convertible  = (*FetchBlocksHeaders)(nil)
+	_ conv.Serializable = (*FetchBlocksHeaders)(nil)
+	_ conv.Convertible  = (*SyncBlocks)(nil)
+	_ conv.Serializable = (*SyncBlocks)(nil)
 )
 
 // LocateHeaders includes headers sended to a peer to locate checkpoint
@@ -46,35 +60,12 @@ type SyncCheckHash struct {
 // FetchBlocksHeaders includes headers sended to a sync peer to
 // fetch blocks
 type FetchBlocksHeaders struct {
-	Headers []*crypto.HashType
+	LocateHeaders
 }
 
 // SyncBlocks includes blocks sended from synchronized peer to local node
 type SyncBlocks struct {
 	Blocks []*coreTypes.Block
-}
-
-// ConvHashesToBytesArray convert []*crypto.HashType to [][]byte
-func ConvHashesToBytesArray(hashes []*crypto.HashType) [][]byte {
-	bytesArray := make([][]byte, 0, len(hashes))
-	for _, v := range hashes {
-		bytesArray = append(bytesArray, (*v)[:])
-	}
-	return bytesArray
-}
-
-// ConvBytesArrayToHashes convert [][]byte to []*crypto.HashType
-func ConvBytesArrayToHashes(bytesArray [][]byte) ([]*crypto.HashType, error) {
-	hashes := make([]*crypto.HashType, len(bytesArray))
-	for i, v := range bytesArray {
-		if len(v) != crypto.HashSize {
-			return nil, fmt.Errorf("netsync FromProtoMessage] LocateHeaders contains " +
-				"hash whoes length is not HashSize(32) bytes")
-		}
-		hashes[i] = &crypto.HashType{}
-		copy(hashes[i][:], v[:])
-	}
-	return hashes, nil
 }
 
 // ToProtoMessage converts LocateHeaders to proto message.
@@ -113,4 +104,244 @@ func (lh *LocateHeaders) Unmarshal(data []byte) error {
 		return err
 	}
 	return lh.FromProtoMessage(msg)
+}
+
+// ToProtoMessage converts SyncHeaders to proto message.
+func (sh *SyncHeaders) ToProtoMessage() (proto.Message, error) {
+	headers, err := ConvHeadersToPbHeaders(sh.Headers)
+	if err != nil {
+		return nil, err
+	}
+	return &netsyncpb.SyncHeaders{
+		Headers: headers,
+	}, nil
+}
+
+// FromProtoMessage converts proto message to SyncHeaders
+func (sh *SyncHeaders) FromProtoMessage(message proto.Message) error {
+	if m, ok := message.(*netsyncpb.SyncHeaders); ok {
+		if m != nil {
+			var err error
+			sh.Headers, err = ConvPbHeadersToHeaders(m.Headers)
+			if err != nil {
+				logger.Info(err.Error())
+				return ErrInvalidProtoMessage
+			}
+			return nil
+		}
+		return ErrEmptyProtoMessage
+	}
+	return ErrInvalidProtoMessage
+}
+
+// Marshal method marshal SyncHeaders object to binary
+func (sh *SyncHeaders) Marshal() (data []byte, err error) {
+	return conv.MarshalConvertible(sh)
+}
+
+// Unmarshal method unmarshal binary data to SyncHeaders object
+func (sh *SyncHeaders) Unmarshal(data []byte) error {
+	msg := &netsyncpb.SyncHeaders{}
+	if err := proto.Unmarshal(data, msg); err != nil {
+		return err
+	}
+	return sh.FromProtoMessage(msg)
+}
+
+// ToProtoMessage converts CheckHash to proto message.
+func (ch *CheckHash) ToProtoMessage() (proto.Message, error) {
+	pbCh := new(netsyncpb.CheckHash)
+	pbCh.BeginHash = make([]byte, crypto.HashSize)
+	copy(pbCh.BeginHash[:], (*ch.BeginHash)[:])
+	pbCh.Length = ch.Length
+	return pbCh, nil
+}
+
+// FromProtoMessage converts proto message to CheckHash
+func (ch *CheckHash) FromProtoMessage(message proto.Message) error {
+	if m, ok := message.(*netsyncpb.CheckHash); ok {
+		if m != nil {
+			copy((ch.BeginHash)[:], m.BeginHash[:])
+			ch.Length = m.Length
+			return nil
+		}
+		return ErrEmptyProtoMessage
+	}
+	return ErrInvalidProtoMessage
+}
+
+// Marshal method marshal CheckHash object to binary
+func (ch *CheckHash) Marshal() (data []byte, err error) {
+	return conv.MarshalConvertible(ch)
+}
+
+// Unmarshal method unmarshal binary data to CheckHash object
+func (ch *CheckHash) Unmarshal(data []byte) error {
+	msg := &netsyncpb.CheckHash{}
+	if err := proto.Unmarshal(data, msg); err != nil {
+		return err
+	}
+	return ch.FromProtoMessage(msg)
+}
+
+// ToProtoMessage converts SyncCheckHash to proto message.
+func (sch *SyncCheckHash) ToProtoMessage() (proto.Message, error) {
+	pbSch := new(netsyncpb.SyncCheckHash)
+	pbSch.RootHash = make([]byte, crypto.HashSize)
+	copy(pbSch.RootHash[:], (*sch.RootHash)[:])
+	return pbSch, nil
+}
+
+// FromProtoMessage converts proto message to SyncCheckHash
+func (sch *SyncCheckHash) FromProtoMessage(message proto.Message) error {
+	if m, ok := message.(*netsyncpb.SyncCheckHash); ok {
+		if m != nil {
+			copy((sch.RootHash)[:], m.RootHash[:])
+			return nil
+		}
+		return ErrEmptyProtoMessage
+	}
+	return ErrInvalidProtoMessage
+}
+
+// Marshal method marshal SyncCheckHash object to binary
+func (sch *SyncCheckHash) Marshal() (data []byte, err error) {
+	return conv.MarshalConvertible(sch)
+}
+
+// Unmarshal method unmarshal binary data to SyncCheckHash object
+func (sch *SyncCheckHash) Unmarshal(data []byte) error {
+	msg := &netsyncpb.SyncCheckHash{}
+	if err := proto.Unmarshal(data, msg); err != nil {
+		return err
+	}
+	return sch.FromProtoMessage(msg)
+}
+
+// ToProtoMessage converts SyncBlocks to proto message.
+func (sb *SyncBlocks) ToProtoMessage() (proto.Message, error) {
+	blocks, err := ConvBlocksToPbBlocks(sb.Blocks)
+	if err != nil {
+		return nil, err
+	}
+	return &netsyncpb.SyncBlocks{Blocks: blocks}, nil
+}
+
+// FromProtoMessage converts proto message to SyncBlocks
+func (sb *SyncBlocks) FromProtoMessage(message proto.Message) error {
+	if m, ok := message.(*netsyncpb.SyncBlocks); ok {
+		if m != nil {
+			var err error
+			sb.Blocks, err = ConvPbBlocksToBlocks(m.Blocks)
+			if err != nil {
+				logger.Info(err.Error())
+				return ErrInvalidProtoMessage
+			}
+			return nil
+		}
+		return ErrEmptyProtoMessage
+	}
+	return ErrInvalidProtoMessage
+}
+
+// Marshal method marshal SyncBlocks object to binary
+func (sb *SyncBlocks) Marshal() (data []byte, err error) {
+	return conv.MarshalConvertible(sb)
+}
+
+// Unmarshal method unmarshal binary data to SyncBlocks object
+func (sb *SyncBlocks) Unmarshal(data []byte) error {
+	msg := &netsyncpb.SyncBlocks{}
+	if err := proto.Unmarshal(data, msg); err != nil {
+		return err
+	}
+	return sb.FromProtoMessage(msg)
+}
+
+// ConvHashesToBytesArray convert []*crypto.HashType to [][]byte
+func ConvHashesToBytesArray(hashes []*crypto.HashType) [][]byte {
+	bytesArray := make([][]byte, len(hashes))
+	for i, v := range hashes {
+		bytesArray[i] = make([]byte, crypto.HashSize)
+		copy(bytesArray[i][:], (*v)[:])
+	}
+	return bytesArray
+}
+
+// ConvBytesArrayToHashes convert [][]byte to []*crypto.HashType
+func ConvBytesArrayToHashes(bytesArray [][]byte) ([]*crypto.HashType, error) {
+	hashes := make([]*crypto.HashType, len(bytesArray))
+	for i, v := range bytesArray {
+		if len(v) != crypto.HashSize {
+			return nil, fmt.Errorf("netsync FromProtoMessage] LocateHeaders contains " +
+				"hash whoes length is not HashSize(32) bytes")
+		}
+		hashes[i] = &crypto.HashType{}
+		copy(hashes[i][:], v[:])
+	}
+	return hashes, nil
+}
+
+// ConvHeadersToPbHeaders convert []*coreTypes.BlockHeader to
+// []*corepb.BlockHeader
+func ConvHeadersToPbHeaders(headers []*coreTypes.BlockHeader) (
+	[]*corepb.BlockHeader, error) {
+	pbHeaders := make([]*corepb.BlockHeader, 0, len(headers))
+	for _, v := range headers {
+		msg, err := v.ToProtoMessage()
+		if err != nil {
+			return nil, err
+		}
+		hmsg, ok := msg.(*corepb.BlockHeader)
+		if !ok {
+			return nil, fmt.Errorf("asserted failed for proto.Message to *corepb.BlockHeader")
+		}
+		pbHeaders = append(pbHeaders, hmsg)
+	}
+	return pbHeaders, nil
+}
+
+// ConvPbHeadersToHeaders convert []*corepb.BlockHeader to
+// []*coreTypes.BlockHeader
+func ConvPbHeadersToHeaders(pbHeaders []*corepb.BlockHeader) (
+	[]*coreTypes.BlockHeader, error) {
+	headers := make([]*coreTypes.BlockHeader, 0, len(pbHeaders))
+	for _, v := range pbHeaders {
+		h := new(coreTypes.BlockHeader)
+		if err := h.FromProtoMessage(v); err != nil {
+			return nil, err
+		}
+		headers = append(headers, h)
+	}
+	return headers, nil
+}
+
+// ConvBlocksToPbBlocks convert []*coreTypes.Block to []*corepb.Block
+func ConvBlocksToPbBlocks(blocks []*coreTypes.Block) ([]*corepb.Block, error) {
+	pbBlocks := make([]*corepb.Block, 0, len(blocks))
+	for _, v := range blocks {
+		msg, err := v.ToProtoMessage()
+		if err != nil {
+			return nil, err
+		}
+		hmsg, ok := msg.(*corepb.Block)
+		if !ok {
+			return nil, fmt.Errorf("asserted failed for proto.Message to *corepb.Block")
+		}
+		pbBlocks = append(pbBlocks, hmsg)
+	}
+	return pbBlocks, nil
+}
+
+// ConvPbHeadersToHeaders convert []*corepb.Block to []*coreTypes.Block
+func ConvPbBlocksToBlocks(pbBlocks []*corepb.Block) ([]*coreTypes.Block, error) {
+	blocks := make([]*coreTypes.Block, 0, len(pbBlocks))
+	for _, v := range pbBlocks {
+		h := new(coreTypes.Block)
+		if err := h.FromProtoMessage(v); err != nil {
+			return nil, err
+		}
+		blocks = append(blocks, h)
+	}
+	return blocks, nil
 }
--- netsync/types_test.go
+++ netsync/types_test.go
@@ -40,3 +40,38 @@ func TestLocateHeaders(t *testing.T) {
 		t.Fatalf("want: %+v, got: %+v", locateHeaders, gotLocateHeaders)
 	}
 }
+
+func TestLocateHeaders(t *testing.T) {
+	locateHeaders := LocateHeaders{
+		Headers: []*crypto.HashType{
+			&crypto.HashType{0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa,
+				0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16,
+				0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f},
+			&crypto.HashType{0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa,
+				0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16,
+				0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x10, 0x0a},
+		},
+	}
+	data, err := locateHeaders.Marshal()
+	if err != nil {
+		t.Fatal(err)
+	}
+	//t.Log(data)
+	//data = []byte{10, 32, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+	//	16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31}
+	//data = []byte{10, 31, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+	//	16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30}
+	gotLocateHeaders := new(LocateHeaders)
+	err = gotLocateHeaders.Unmarshal(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if *gotLocateHeaders.Headers[0] != [crypto.HashSize]byte{
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+		16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31} ||
+		*gotLocateHeaders.Headers[1] != [crypto.HashSize]byte{
+			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+			16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 16, 10} {
+		t.Fatalf("want: %+v, got: %+v", locateHeaders, gotLocateHeaders)
+	}
+}
