9fa3c8be6e2c785fb5bdc6ff9edae690a0746649
Leon <leon@castbox.fm>
2018-10-23 17:02:40 +0800

    fix blockchain test case

--- core/chain/blockchain_test.go
+++ core/chain/blockchain_test.go
@@ -121,7 +121,7 @@ func TestBlockProcessing(t *testing.T) {
 	// b0 -> b1 -> b2  -> b3  -> b4
 	//		   \-> b2A -> b3A
 	b3 := nextBlock(b2)
-	verifyProcessBlock(t, b3, false, false, nil, 3, b3A)
+	verifyProcessBlock(t, b3, false, false, core.ErrBlockExists, 3, b3A)
 	b4 := nextBlock(b3)
 	verifyProcessBlock(t, b4, true, false, nil, 4, b4)
 
@@ -130,7 +130,7 @@ func TestBlockProcessing(t *testing.T) {
 	//						  \-> b4B -> b5B
 	//		   \-> b2A -> b3A
 	b4B := nextBlock(b3)
-	verifyProcessBlock(t, b4B, false, false, nil, 4, b4)
+	verifyProcessBlock(t, b4B, false, false, core.ErrBlockExists, 4, b4)
 	b5B := nextBlock(b4B)
 	verifyProcessBlock(t, b5B, true, false, nil, 5, b5B)
 
