e247eb03a166d585639daba51defda3253bab60d
xujingshi <910829229@qq.com>
2018-10-25 16:26:23 +0800

    del p2p/dynamicbanscore.go

--- p2p/dynamicpeerscore.go
+++ p2p/dynamicpeerscore.go
@@ -1,4 +1,8 @@
-package main
+// Copyright (c) 2018 ContentBox Authors.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package p2p
 
 import (
 	"fmt"
