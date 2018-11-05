419002aa09a4b709787ab7439f630e98c82c396e
xujingshi <910829229@qq.com>
2018-11-05 19:05:00 +0800

    notifier add lru cache finish

--- p2p/notifier.go
+++ p2p/notifier.go
@@ -5,16 +5,31 @@
 package p2p
 
 import (
+	"crypto/sha256"
 	"sync"
 
+	"github.com/BOXFoundation/boxd/crypto"
+	lru "github.com/hashicorp/golang-lru"
 	"github.com/jbenet/goprocess"
 )
+
+const (
+	repeatableMsg = iota
+	uniqueBodyMsg
+	uniqueBodyMsgPerPeer
+)
+
+var msgTypeToFilter = map[uint32]uint32{
+	NewBlockMsg:    uniqueBodyMsg,
+	ChainUpdateMsg: uniqueBodyMsgPerPeer,
+}
 
 // Notifier dispatcher & distribute business message.
 type Notifier struct {
 	notifierMap *sync.Map
 	proc        goprocess.Process
 	receiveCh   chan Message
+	cache       *lru.Cache
 }
 
 // Notifiee represent message receiver.
@@ -29,6 +44,7 @@ func NewNotifier() *Notifier {
 		notifierMap: new(sync.Map),
 		receiveCh:   make(chan Message, 65536),
 	}
+	notifier.cache, _ = lru.New(512)
 	return notifier
 }
 
@@ -68,5 +84,32 @@ func (notifier *Notifier) Loop(parent goprocess.Process) {
 
 // Notify message to notifier
 func (notifier *Notifier) Notify(msg Message) {
-	notifier.receiveCh <- msg
+	notify := notifier.filter(msg)
+	if notify {
+		notifier.receiveCh <- msg
+	}
+}
+
+func (notifier *Notifier) filter(msg Message) bool {
+	if msgTypeToFilter[msg.Code()] == repeatableMsg {
+		return true
+	}
+	key := notifier.lruKey(msg)
+	if notifier.cache.Contains(key) {
+		return false
+	}
+	notifier.cache.Add(key, msg)
+	return true
+}
+
+func (notifier *Notifier) lruKey(msg Message) crypto.HashType {
+	key := []byte(msg.Body())
+
+	if msgTypeToFilter[msg.Code()] == uniqueBodyMsgPerPeer {
+		key = append(key, msg.From()...)
+	}
+
+	hash := sha256.Sum256(msg.Body())
+
+	return hash
 }
