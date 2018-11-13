370c85320a520bfe2b7d7c3119a0910a9e5e55b5
Jerry Peen <zhiguo.ping@castbox.fm>
2018-11-13 19:06:58 +0800

    chore(tests): add flag to switch start nodes

--- tests/main.go
+++ tests/main.go
@@ -30,15 +30,26 @@ var logger = log.NewLogger("tests") // logger
 
 var (
 	peersAddr = []string{
-		"127.0.0.1:19191",
-		"127.0.0.1:19181",
-		"127.0.0.1:19171",
-		"127.0.0.1:19161",
-		"127.0.0.1:19151",
-		"127.0.0.1:19141",
+		// boxd001
+		"127.0.0.1:19111", // n1
+		"127.0.0.1:19121", // n2
+		"127.0.0.1:19131", // n3
+		//"192.168.0.227:19111", // n1
+		//"192.168.0.227:19121", // n2
+		//"192.168.0.227:19131", // n3
+		//"39.105.210.220:19001", // s1
+		//"39.105.210.220:19011", // s2
+		// boxd002
+		//"39.105.214.10:19311", // a1
+		//"39.105.214.10:19321", // a2
+		//"192.168.0.226:19141", // n4
+		//"192.168.0.226:19151", // n5
+		//"192.168.0.226:19161", // n6
 	}
 
 	enableDocker = flag.Bool("docker", false, "test on docker?")
+
+	newNodes = flag.Bool("nodes", false, "test on docker?")
 )
 
 func init() {
@@ -47,23 +58,25 @@ func init() {
 
 func main() {
 	flag.Parse()
-	// prepare environment and clean history data
-	if err := prepareEnv(peerCount); err != nil {
-		logger.Panic(err)
-	}
-	defer tearDown(peerCount)
-
-	// start nodes
-	if *enableDocker {
-		if err := startNodes(); err != nil {
+	if *newNodes {
+		// prepare environment and clean history data
+		if err := prepareEnv(peerCount); err != nil {
 			logger.Panic(err)
 		}
-		defer stopNodes()
-	} else {
-		processes, err := startLocalNodes(peerCount)
-		defer stopLocalNodes(processes...)
-		if err != nil {
-			logger.Panic(err)
+		defer tearDown(peerCount)
+
+		// start nodes
+		if *enableDocker {
+			if err := startNodes(); err != nil {
+				logger.Panic(err)
+			}
+			defer stopNodes()
+		} else {
+			processes, err := startLocalNodes(peerCount)
+			defer stopLocalNodes(processes...)
+			if err != nil {
+				logger.Panic(err)
+			}
 		}
 	}
 
