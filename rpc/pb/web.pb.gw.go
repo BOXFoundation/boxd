72334342b0023078bd7222ea5cbb30d1cbf19f9a
gtygo <gtydoit@gmail.com>
2019-05-17 16:38:16 +0800

    add web.pb.go & web.pb.gw.go

--- rpc/pb/web.pb.gw.go
+++ rpc/pb/web.pb.gw.go
@@ -62,6 +62,23 @@ func request_WebApi_ViewBlockDetail_0(ctx context.Context, marshaler runtime.Mar
 
 }
 
+func request_WebApi_DoCall_0(ctx context.Context, marshaler runtime.Marshaler, client WebApiClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CallReq
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.DoCall(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterWebApiHandlerFromEndpoint is same as RegisterWebApiHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterWebApiHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -140,6 +157,26 @@ func RegisterWebApiHandlerClient(ctx context.Context, mux *runtime.ServeMux, cli
 
 	})
 
+	mux.Handle("POST", pattern_WebApi_DoCall_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_WebApi_DoCall_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_WebApi_DoCall_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -147,10 +184,14 @@ var (
 	pattern_WebApi_ViewTxDetail_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "tx", "detail"}, ""))
 
 	pattern_WebApi_ViewBlockDetail_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "block", "detail"}, ""))
+
+	pattern_WebApi_DoCall_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "contract", "call"}, ""))
 )
 
 var (
 	forward_WebApi_ViewTxDetail_0 = runtime.ForwardResponseMessage
 
 	forward_WebApi_ViewBlockDetail_0 = runtime.ForwardResponseMessage
+
+	forward_WebApi_DoCall_0 = runtime.ForwardResponseMessage
 )
