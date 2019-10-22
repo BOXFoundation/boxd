3696618545083a0aef379de573bc2f71e965a733
Leon <leon@castbox.fm>
2019-10-22 14:36:57 +0800

    bugfix in bpos

--- consensus/bpos/bpos.go
+++ consensus/bpos/bpos.go
@@ -835,7 +835,7 @@ func (bpos *Bpos) verifySign(block *types.Block) (bool, error) {
 
 func (bpos *Bpos) verifyDynastySwitch(block *types.Block) error {
 
-	if (uint64((block.Header.Height+1))%bpos.context.verifyDynastySwitchThreshold.Uint64() == 0) || (uint64((block.Header.Height))%bpos.context.verifyCalcScoreThreshold.Uint64() == 0) { // dynasty switch
+	if (uint64((block.Header.Height+1))%bpos.context.verifyDynastySwitchThreshold.Uint64() == 0) || (uint64((block.Header.Height))%bpos.context.verifyDynastySwitchThreshold.Uint64() == bpos.context.verifyCalcScoreThreshold.Uint64()) { // dynasty switch
 		if !chain.IsInternalContract(block.Txs[1]) {
 			return ErrInvalidDynastySwitchTx
 		}
