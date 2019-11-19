9e98924d274ec5344d8a9ba3e8fce0fb24643a33
Jerry Peen <zhiguo.ping@castbox.fm>
2019-11-19 15:45:58 +0800

    fix(contract): new contract address with nonce added 1 in evm.Create

--- vm/evm.go
+++ vm/evm.go
@@ -439,7 +439,7 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 func (evm *EVM) Create(
 	caller ContractRef, code []byte, gas uint64, value *big.Int, interpreterInvoke bool,
 ) (ret []byte, contractAddr coretypes.AddressHash, leftOverGas uint64, err error) {
-	contractAddr = *coretypes.CreateAddress(caller.Address(), evm.StateDB.GetNonce(caller.Address()))
+	contractAddr = *coretypes.CreateAddress(caller.Address(), evm.StateDB.GetNonce(caller.Address())+1)
 	return evm.create(caller, &codeAndHash{code: code}, gas, value, contractAddr, interpreterInvoke)
 }
 
